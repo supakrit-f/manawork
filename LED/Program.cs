fbb39fc663e1eff5daafb03b8861c1bd143b041e
supakrit-f <56065486+supakrit-f@users.noreply.github.com>
2020-05-07 19:25:05 +0700

    Add files via upload

---
+++ LED/Program.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            string LedNo1 = " ";
+            string LedNo2 = " ";
+            string LedNo3 = " ";
+            string LedNo4 = " ";
+            string LedNo5 = " ";
+            string LedNo6 = " ";
+            string LedNo7 = " ";
+            string LedNo8 = " ";
+            string LedNo9 = " ";
+            string LedNoA = " ";
+            Console.WriteLine("1[ " + LedNo1 + " ] 2[ " + LedNo2 + " ] 3[ " + LedNo3 + " ] " +
+                    "4[ " + LedNo4 + " ] 5[ " + LedNo5 + "] 6[ " + LedNo6 + " ] 7[ " + LedNo7 + " ]" +
+                    " 8[ " + LedNo8 + " ] 9[ " + LedNo9 + " ] A[ " + LedNoA + " ]");
+            do
+            {
+                Console.WriteLine("Please choose LED to turn On/Off: ");
+                string i = Console.ReadLine();
+                if (i == "1")
+                {
+                    if (LedNo1 == " ")
+                    {
+                        LedNo1 = "!";
+                    }
+                    else
+                    {
+                        LedNo1 = " ";
+                    }
+                }
+                if (i == "2")
+                {
+                    if (LedNo2 == " ")
+                    {
+                        LedNo2 = "!";
+                    }
+                    else
+                    {
+                        LedNo2 = " ";
+                    }
+                }
+                if (i == "3")
+                {
+                    if (LedNo3 == " ")
+                    {
+                        LedNo3 = "!";
+                    }
+                    else
+                    {
+                        LedNo3 = " ";
+                    }
+                }
+                if (i == "4")
+                {
+                    if (LedNo4 == " ")
+                    {
+                        LedNo4 = "!";
+                    }
+                    else
+                    {
+                        LedNo4 = " ";
+                    }
+                }
+                if (i == "5")
+                {
+                    if (LedNo5 == " ")
+                    {
+                        LedNo5 = "!";
+                    }
+                    else
+                    {
+                        LedNo5 = " ";
+                    }
+                }
+                if (i == "6")
+                {
+                    if (LedNo6 == " ")
+                    {
+                        LedNo6 = "!";
+                    }
+                    else
+                    {
+                        LedNo6 = " ";
+                    }
+                }
+                if (i == "7")
+                {
+                    if (LedNo7 == " ")
+                    {
+                        LedNo7 = "!";
+                    }
+                    else
+                    {
+                        LedNo7 = " ";
+                    }
+                }
+                if (i == "8")
+                {
+                    if (LedNo8 == " ")
+                    {
+                        LedNo8 = "!";
+                    }
+                    else
+                    {
+                        LedNo8 = " ";
+                    }
+                }
+                if (i == "9")
+                {
+                    if (LedNo9 == " ")
+                    {
+                        LedNo9 = "!";
+                    }
+                    else
+                    {
+                        LedNo9 = " ";
+                    }
+                }
+                if (i == "A")
+                {
+                    if (LedNoA == " ")
+                    {
+                        LedNoA = "!";
+                    }
+                    else
+                    {
+                        LedNoA = " ";
+                    }
+                }
+                Console.WriteLine("1[ " + LedNo1 + " ] 2[ " + LedNo2 + " ] 3[ " + LedNo3 + " ] " +
+                    "4[ " + LedNo4 + " ] 5[ " + LedNo5 + "] 6[ " + LedNo6 + " ] 7[ " + LedNo7 + " ]" +
+                    " 8[ " + LedNo8 + " ] 9[ " + LedNo9 + " ] A[ " + LedNoA + " ]");
+
+            } while (count == 1);
+        }
+    }
+}
---
+++ cart/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace homework9
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Count = 0;
+            string path = @"D:\product.csv";
+            string line;
+            double total = 0.00;
+            int i = 0;
+            int j = 0;
+            int e = 0;
+            ArrayList productList = new ArrayList();
+            var sku = new List<String>();
+            var name = new List<String>();
+            var price = new List<String>();
+            ArrayList product = new ArrayList();
+            using (StreamReader reader = new StreamReader(path, Encoding.Default, true))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    product.Add(line);
+                    var values = line.Split(",");
+
+                    sku.Add(values[0]);
+                    name.Add(values[1]);
+                    price.Add(values[2]);
+                }
+            }
+            var SKU = sku.ToArray();
+            var Name = name.ToArray();
+            var Price = price.ToArray();
+            String idProduct = " ";
+            Console.WriteLine("Products in your cart are");
+            Console.WriteLine("none\n---");
+            Console.WriteLine("Total amount: 0 baht");
+            do
+            {
+                Console.WriteLine("please input a product key: ");
+                idProduct = Console.ReadLine();
+                for (i = 1; i < SKU.Length; i++)
+                {
+                    if (idProduct == SKU[i])
+                    {
+                        j++;
+                        productList.Add(j + ". " + Name[i] + "    " + price[i]);
+                        total = total + int.Parse(price[i]);
+                    }                  
+                }
+               
+                Console.WriteLine("Product in your cart are");
+                foreach(var item in productList){
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("-------");
+                Console.WriteLine("Total amount: " + total + " baht");
+            } while (Count == 0);
+        }
+    }
+}
